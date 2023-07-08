fc4291c40816231f834a0537240d64ccdbd47a1e
Zarex <sr1056@outlook.com>
2023-07-08 17:02:22 -0400

    WadLauncher.bat-Initial beta
    Working batch file to load doom wads with their respective IWADS, but very basic and lacks automation

---
+++ WadLauncher.bat
@@ -0,0 +1,30 @@
+@echo off
+
+REM Titre de la fenêtre et affichage
+title Doom mod launcher
+echo ----------------------------------------------------------------------
+echo Zarex's Doom mod launcher
+echo ----------------------------------------------------------------------
+
+
+REM Demander à l'utilisateur d'entrer un IWAD
+:iwaderror
+echo Which iwad do you want to use?
+	dir wad /b	
+set /p iwad=Enter your choice here : 
+	if exist wad\%iwad% (cd wad\%iwad%) else goto iwaderror
+	
+REM Affichage des mods disponibles
+:waderror
+cls
+echo ----------------------------------------------------------------------
+echo Here are the wads available : 
+	dir /b
+
+REM Demander à l'utilisateur d'entrer le programme qu'il veut installer
+echo ----------------------------------------------------------------------
+	set /p input=Enter your choice here (Enter file name with extension) : 
+
+REM Si le dossier entré par l'utilisateur est valide, exécuter l'installeur
+	if exist %input% (start ../../gzdoom.exe -iwad %iwad% -file %input%) else (goto waderror)
+	exit
